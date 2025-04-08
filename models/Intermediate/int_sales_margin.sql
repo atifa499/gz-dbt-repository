e2b88e3634cd3251321e08636115f4e1a4d4bb07
Atifa Bououni <bouounilisa4@gmail.com>
2025-04-08 18:49:28 +0000

    Orders changes

---
+++ models/Intermediate/int_sales_margin.sql
@@ -0,0 +1,21 @@
+
+
+
+
+SELECT
+p.products_id,
+p.purchase_price,
+s.date_date,
+s.orders_id,
+s.revenue,
+s.quantity,
+(p.purchase_price * s.quantity) AS purchase_cost,
+((s.revenue - s.quantity) * p.purchase_price) AS margin
+FROM {{ref("stg_raw__product")}} p
+JOIN {{ref("stg_raw__sales")}} s
+ON p.products_id = s.products_id
+
+
+
+
+
